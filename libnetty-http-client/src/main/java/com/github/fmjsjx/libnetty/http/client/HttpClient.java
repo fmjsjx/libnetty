3dd1b13b6d8e4161012b7317af9a1cbf550678ed
Fang MJ <fmjsjx@163.com>
2020-05-05 20:56:34 +0800

    add project libnetty-http-client

---
+++ libnetty-http-client/src/main/java/com/github/fmjsjx/libnetty/http/client/HttpClient.java
@@ -0,0 +1,22 @@
+package com.github.fmjsjx.libnetty.http.client;
+
+/**
+ * Main interface for an HTTP client.
+ * 
+ * @author fmjsjx
+ * 
+ * @since 1.0
+ */
+public interface HttpClient extends AutoCloseable {
+
+    /**
+     * Close this HTTP client.
+     * 
+     * @since 1.0
+     */
+    @Override
+    default void close() {
+        // default do nothing
+    }
+
+}
