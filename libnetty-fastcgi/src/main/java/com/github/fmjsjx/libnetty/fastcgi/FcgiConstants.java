34c217ab629dc7155a6a2200343ce34fbe2bbd15
fmjsjx <fmjsjx@163.com>
2020-05-20 12:05:35 +0800

    Add FastCGI record type constants.

--- libnetty-fastcgi/src/main/java/com/github/fmjsjx/libnetty/fastcgi/FcgiConstants.java
+++ libnetty-fastcgi/src/main/java/com/github/fmjsjx/libnetty/fastcgi/FcgiConstants.java
@@ -26,6 +26,51 @@ final class FcgiConstants {
     static final int FCGI_REQUEST_ID_LIMIT = 65536;
 
     /**
+     * {@code FCGI_BEGIN_REQUEST 1}
+     */
+    static final byte FCGI_BEGIN_REQUEST = 1;
+    /**
+     * {@code FCGI_ABORT_REQUEST 2}
+     */
+    static final byte FCGI_ABORT_REQUEST = 2;
+    /**
+     * {@code FCGI_END_REQUEST 3}
+     */
+    static final byte FCGI_END_REQUEST = 3;
+    /**
+     * {@code FCGI_PARAMS 4}
+     */
+    static final byte FCGI_PARAMS = 4;
+    /**
+     * {@code FCGI_STDIN 5}
+     */
+    static final byte FCGI_STDIN = 5;
+    /**
+     * {@code FCGI_STDOUT 6}
+     */
+    static final byte FCGI_STDOUT = 6;
+    /**
+     * {@code FCGI_STDERR 7}
+     */
+    static final byte FCGI_STDERR = 7;
+    /**
+     * {@code FCGI_DATA 8}
+     */
+    static final byte FCGI_DATA = 8;
+    /**
+     * {@code FCGI_GET_VALUES 9}
+     */
+    static final byte FCGI_GET_VALUES = 9;
+    /**
+     * {@code FCGI_GET_VALUES_RESULT 10}
+     */
+    static final byte FCGI_GET_VALUES_RESULT = 10;
+    /**
+     * {@code FCGI_UNKNOWN_TYPE 11}
+     */
+    static final byte FCGI_UNKNOWN_TYPE = 11;
+
+    /**
      * Mask for flags component of FCGI_BeginRequestBody
      */
     static final byte FCGI_KEEP_CONN = 1;
--- libnetty-fastcgi/src/main/java/com/github/fmjsjx/libnetty/fastcgi/FcgiRecordType.java
+++ libnetty-fastcgi/src/main/java/com/github/fmjsjx/libnetty/fastcgi/FcgiRecordType.java
@@ -1,5 +1,7 @@
 package com.github.fmjsjx.libnetty.fastcgi;
 
+import static com.github.fmjsjx.libnetty.fastcgi.FcgiConstants.*;
+
 import java.util.stream.Stream;
 
 /**
@@ -14,43 +16,48 @@ public class FcgiRecordType {
     /**
      * {@code FCGI_BEGIN_REQUEST 1}
      */
-    public static final FcgiRecordType BEGIN_REQUEST = new FcgiRecordType("BEGIN_REQUEST", 1);
+    public static final FcgiRecordType BEGIN_REQUEST = new FcgiRecordType("BEGIN_REQUEST", FCGI_BEGIN_REQUEST);
     /**
      * {@code FCGI_ABORT_REQUEST 2}
      */
-    public static final FcgiRecordType ABORT_REQUEST = new FcgiRecordType("ABORT_REQUEST", 2);
+    public static final FcgiRecordType ABORT_REQUEST = new FcgiRecordType("ABORT_REQUEST", FCGI_ABORT_REQUEST);
     /**
      * {@code FCGI_END_REQUEST 3}
      */
-    public static final FcgiRecordType END_REQUEST = new FcgiRecordType("END_REQUEST", 3);
+    public static final FcgiRecordType END_REQUEST = new FcgiRecordType("END_REQUEST", FCGI_END_REQUEST);
     /**
      * {@code FCGI_PARAMS 4}
      */
-    public static final FcgiRecordType PARAMS = new FcgiRecordType("PARAMS", 4);
+    public static final FcgiRecordType PARAMS = new FcgiRecordType("PARAMS", FCGI_PARAMS);
     /**
      * {@code FCGI_STDIN 5}
      */
-    public static final FcgiRecordType STDIN = new FcgiRecordType("STDIN", 5);
+    public static final FcgiRecordType STDIN = new FcgiRecordType("STDIN", FCGI_STDIN);
     /**
      * {@code FCGI_STDOUT 6}
      */
-    public static final FcgiRecordType STDOUT = new FcgiRecordType("STDOUT", 6);
+    public static final FcgiRecordType STDOUT = new FcgiRecordType("STDOUT", FCGI_STDOUT);
     /**
      * {@code FCGI_STDERR 7}
      */
-    public static final FcgiRecordType STDERR = new FcgiRecordType("STDERR", 7);
+    public static final FcgiRecordType STDERR = new FcgiRecordType("STDERR", FCGI_STDERR);
     /**
      * {@code FCGI_DATA 8}
      */
-    public static final FcgiRecordType DATA = new FcgiRecordType("DATA", 8);
+    public static final FcgiRecordType DATA = new FcgiRecordType("DATA", FCGI_DATA);
     /**
      * {@code FCGI_GET_VALUES 9}
      */
-    public static final FcgiRecordType GET_VALUES = new FcgiRecordType("GET_VALUES", 9);
+    public static final FcgiRecordType GET_VALUES = new FcgiRecordType("GET_VALUES", FCGI_GET_VALUES);
     /**
      * {@code FCGI_GET_VALUES_RESULT 10}
      */
-    public static final FcgiRecordType GET_VALUES_RESULT = new FcgiRecordType("GET_VALUES_RESULT", 10);
+    public static final FcgiRecordType GET_VALUES_RESULT = new FcgiRecordType("GET_VALUES_RESULT",
+            FCGI_GET_VALUES_RESULT);
+    /**
+     * {@code FCGI_UNKNOWN_TYPE 11}
+     */
+    public static final FcgiRecordType UNKNOWN_TYPE = new FcgiRecordType("UNKNOWN_TYPE", FCGI_UNKNOWN_TYPE);
 
     private static final FcgiRecordType[] values;
 
@@ -69,15 +76,21 @@ public class FcgiRecordType {
         if (type > 0 && type < values.length) {
             return values[type];
         }
-        return new FcgiRecordType("UNKNOWN_TYPE", type);
+        return new FcgiRecordType("UNKNOWN_TYPE", type, true);
     }
 
     private final String name;
     private final int type;
+    private final boolean unknown;
 
     FcgiRecordType(String name, int type) {
+        this(name, type, false);
+    }
+
+    FcgiRecordType(String name, int type, boolean unknown) {
         this.name = name;
         this.type = type;
+        this.unknown = unknown;
     }
 
     /**
@@ -96,6 +109,15 @@ public class FcgiRecordType {
      */
     public String name() {
         return name;
+    }
+
+    /**
+     * Returns {@code true} if is type is unknown.
+     * 
+     * @return {@code true} if unknown
+     */
+    public boolean isUnknown() {
+        return unknown;
     }
 
     @Override
