713621fe342628ae5d5f7175c1f25a4658ab32c6
fmjsjx <fmjsjx@163.com>
2020-05-22 09:49:46 +0800

    Add commits.

--- libnetty-fastcgi/src/main/java/com/github/fmjsjx/libnetty/fastcgi/FcgiRequest.java
+++ libnetty-fastcgi/src/main/java/com/github/fmjsjx/libnetty/fastcgi/FcgiRequest.java
@@ -22,10 +22,39 @@ public class FcgiRequest extends AbstractReferenceCounted implements FcgiMessage
     private final FcgiStdin stdin;
     private final Optional<FcgiData> data;
 
+    /**
+     * Constructs a new {@link FcgiRequest} instance with {@code responder} role.
+     * 
+     * @param protocolVersion the {@code FcgiVersion}
+     * @param requestId       the request id
+     * @param role            the {@code FcgiRole}
+     * @param stdinContent    the {@code ByteBuf} content of {@code FCGI_STDIN}
+     */
+    public FcgiRequest(FcgiVersion protocolVersion, int requestId, ByteBuf stdinContent) {
+        this(protocolVersion, requestId, FcgiRole.RESPONDER, stdinContent, null);
+    }
+
+    /**
+     * Constructs a new {@link FcgiRequest} instance.
+     * 
+     * @param protocolVersion the {@code FcgiVersion}
+     * @param requestId       the request id
+     * @param role            the {@code FcgiRole}
+     * @param stdinContent    the {@code ByteBuf} content of {@code FCGI_STDIN}
+     */
     public FcgiRequest(FcgiVersion protocolVersion, int requestId, FcgiRole role, ByteBuf stdinContent) {
         this(protocolVersion, requestId, role, stdinContent, null);
     }
 
+    /**
+     * Constructs a new {@link FcgiRequest} instance.
+     * 
+     * @param protocolVersion the {@code FcgiVersion}
+     * @param requestId       the request id
+     * @param role            the {@code FcgiRole}
+     * @param stdinContent    the {@code ByteBuf} content of {@code FCGI_STDIN}
+     * @param dataContent     the {@code ByteBuf} content of {@code FCGI_DATA}
+     */
     public FcgiRequest(FcgiVersion protocolVersion, int requestId, FcgiRole role, ByteBuf stdinContent,
             ByteBuf dataContent) {
         this.protocolVersion = protocolVersion;
@@ -40,26 +69,56 @@ public class FcgiRequest extends AbstractReferenceCounted implements FcgiMessage
         }
     }
 
+    /**
+     * Returns the {@link FcgiVersion}.
+     * 
+     * @return the {@code FcgiVersion}
+     */
     public FcgiVersion protocolVersion() {
         return protocolVersion;
     }
 
+    /**
+     * Returns the request id.
+     * 
+     * @return the request id
+     */
     public int requestId() {
         return requestId;
     }
 
+    /**
+     * Returns the {@code FCGI_BEGIN_REQUEST} record.
+     * 
+     * @return the {@code FcgiBeginRequest}
+     */
     public FcgiBeginRequest beginRequest() {
         return beginRequest;
     }
 
+    /**
+     * Returns the {@code FCGI_PARAMS} record.
+     * 
+     * @return the {@code FcgiParams}
+     */
     public FcgiParams params() {
         return params;
     }
 
+    /**
+     * Returns the {@code FCGI_STDIN} record.
+     * 
+     * @return the {@code FcgiStdin}
+     */
     public FcgiStdin stdin() {
         return stdin;
     }
 
+    /**
+     * Returns the {@code FCGI_DATA} record if present.
+     * 
+     * @return an {@code Optional<FcgiData>}
+     */
     public Optional<FcgiData> data() {
         return data;
     }
