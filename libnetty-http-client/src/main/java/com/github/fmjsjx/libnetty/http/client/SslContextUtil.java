940ae3c1f34ab9f3ab79bc7380f6df0bbb864ef8
fmjsjx <fmjsjx@163.com>
2020-07-24 11:51:46 +0800

    Use SslProvider#OPENSSL_REFCNT when OpenSSL is available.

--- libnetty-http-client/src/main/java/com/github/fmjsjx/libnetty/http/client/SslContextUtil.java
+++ libnetty-http-client/src/main/java/com/github/fmjsjx/libnetty/http/client/SslContextUtil.java
@@ -4,8 +4,10 @@ import javax.net.ssl.SSLException;
 
 import com.github.fmjsjx.libnetty.http.client.exception.HttpRuntimeException;
 
+import io.netty.handler.ssl.OpenSsl;
 import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.SslProvider;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
@@ -26,7 +28,11 @@ public final class SslContextUtil {
      */
     public static final SslContext createForClient() {
         try {
-            return SslContextBuilder.forClient().build();
+            SslContextBuilder builder = SslContextBuilder.forClient();
+            if (OpenSsl.isAvailable()) {
+                builder.sslProvider(SslProvider.OPENSSL_REFCNT);
+            }
+            return builder.build();
         } catch (SSLException e) {
             throw new HttpRuntimeException(e);
         }
