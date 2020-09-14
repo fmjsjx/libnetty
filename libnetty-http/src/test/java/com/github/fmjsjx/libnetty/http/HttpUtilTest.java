04cda2e488cc970a95eea0d2990a854352e47760
fmjsjx <fmjsjx@163.com>
2020-09-14 16:20:05 +0800

    Fix UnitTest for HttpUtil

--- libnetty-http/src/test/java/com/github/fmjsjx/libnetty/http/HttpUtilTest.java
+++ libnetty-http/src/test/java/com/github/fmjsjx/libnetty/http/HttpUtilTest.java
@@ -14,7 +14,7 @@ public class HttpUtilTest {
     @Test
     public void testContentType() {
         AsciiString ct1 = HttpUtil.contentType(HttpHeaderValues.APPLICATION_X_WWW_FORM_URLENCODED);
-        assertEquals("application/x-www-form-urlencoded;charset=UTF-8", ct1.toString());
+        assertEquals("application/x-www-form-urlencoded; charset=UTF-8", ct1.toString());
         AsciiString ct2 = HttpUtil.contentType(HttpHeaderValues.APPLICATION_X_WWW_FORM_URLENCODED);
         assertEquals(ct1, ct2);
         assertTrue(ct1 == ct2);
