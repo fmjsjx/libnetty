75a034457410c60752d40103b1bc3af1057edfe4
fmjsjx <fmjsjx@163.com>
2020-09-16 14:39:29 +0800

    Move in READ_NEXT from AutoReadNextHandler.

--- libnetty-http-server/src/main/java/com/github/fmjsjx/libnetty/http/server/HttpServerHandler.java
+++ libnetty-http-server/src/main/java/com/github/fmjsjx/libnetty/http/server/HttpServerHandler.java
@@ -1,5 +1,8 @@
 package com.github.fmjsjx.libnetty.http.server;
 
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelInboundHandler;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.ChannelHandler.Sharable;
@@ -12,6 +15,12 @@ import io.netty.channel.ChannelHandler.Sharable;
  * @author MJ Fang
  */
 public interface HttpServerHandler extends ChannelInboundHandler {
+
+    /**
+     * A {@link ChannelFutureListener} that calls {@link Channel#read()} of the
+     * {@link Channel} which is associated with the specified {@link ChannelFuture}.
+     */
+    ChannelFutureListener READ_NEXT = f -> f.channel().read();
 
     /**
      * Return {@code true} if the implementation is {@link Sharable} and so can be
