75ad62d5deeec6d456e9259fe196f53cdf939a4f
fmjsjx <fmjsjx@163.com>
2020-09-16 14:38:26 +0800

    Change handler main logic:
    1. Only add READ_NEXT listener when handle FullHttpResponse
    2. Never close channel.

--- libnetty-http-server/src/main/java/com/github/fmjsjx/libnetty/http/server/AutoReadNextHandler.java
+++ libnetty-http-server/src/main/java/com/github/fmjsjx/libnetty/http/server/AutoReadNextHandler.java
@@ -3,11 +3,10 @@ package com.github.fmjsjx.libnetty.http.server;
 import static io.netty.handler.codec.http.HttpUtil.isKeepAlive;
 
 import io.netty.channel.ChannelDuplexHandler;
-import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelPromise;
-import io.netty.handler.codec.http.HttpResponse;
+import io.netty.handler.codec.http.FullHttpResponse;
 
 @Sharable
 class AutoReadNextHandler extends ChannelDuplexHandler {
@@ -20,8 +19,6 @@ class AutoReadNextHandler extends ChannelDuplexHandler {
         return InstanceHolder.instance;
     }
 
-    static ChannelFutureListener READ_NEXT = f -> f.channel().read();
-
     @Override
     public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
         ctx.read();
@@ -29,12 +26,10 @@ class AutoReadNextHandler extends ChannelDuplexHandler {
 
     @Override
     public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
-        if (msg instanceof HttpResponse) {
-            HttpResponse resp = (HttpResponse) msg;
+        if (msg instanceof FullHttpResponse) {
+            FullHttpResponse resp = (FullHttpResponse) msg;
             if (isKeepAlive(resp)) {
-                promise.addListener(READ_NEXT);
-            } else {
-                promise.addListener(ChannelFutureListener.CLOSE);
+                promise.addListener(HttpServerHandler.READ_NEXT);
             }
         }
         super.write(ctx, msg, promise);
