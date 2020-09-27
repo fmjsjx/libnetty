d57f8c0cbb21ccc11af8381ad8fcd9084c74a7f0
fmjsjx <fmjsjx@163.com>
2020-09-27 10:08:05 +0800

    JsonResult => JsonBody

--- libnetty-http-server/src/main/java/com/github/fmjsjx/libnetty/http/server/annotation/JsonBody.java
+++ libnetty-http-server/src/main/java/com/github/fmjsjx/libnetty/http/server/annotation/JsonBody.java
@@ -1,14 +1,15 @@
 package com.github.fmjsjx.libnetty.http.server.annotation;
 
 import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 /**
- * Annotation that indicates a method return value should be bound to the HTTP
- * response body as type {@code "application/json"}.
+ * Annotation that indicates a method return value or a method parameter should
+ * be bound to the HTTP body as type {@code "application/json"}.
  * <p>
  * Supported for {@link HttpRoute} annotated handler methods
  * 
@@ -18,9 +19,9 @@ import java.lang.annotation.Target;
  * 
  * @see HttpRoute
  */
-@Target(METHOD)
+@Target({ METHOD, PARAMETER })
 @Retention(RUNTIME)
-public @interface JsonResult {
+public @interface JsonBody {
 
     String charsetName() default "UTF-8";
 
