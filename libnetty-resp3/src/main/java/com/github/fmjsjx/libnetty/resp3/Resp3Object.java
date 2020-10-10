61229d4b44fd80b7206d056b22a344246621f2be
fmjsjx <fmjsjx@163.com>
2020-10-10 17:51:06 +0800

    Add comment.

--- libnetty-resp3/src/main/java/com/github/fmjsjx/libnetty/resp3/Resp3Object.java
+++ libnetty-resp3/src/main/java/com/github/fmjsjx/libnetty/resp3/Resp3Object.java
@@ -2,6 +2,13 @@ package com.github.fmjsjx.libnetty.resp3;
 
 import com.github.fmjsjx.libnetty.resp.RespObject;
 
+/**
+ * A RESP3 object.
+ * 
+ * @since 1.1
+ *
+ * @author MJ Fang
+ */
 public interface Resp3Object extends RespObject {
 
 }
