760511a3a5ae4a0b06774fe5ca649ae96f8bfe02
fmjsjx <fmjsjx@163.com>
2021-07-07 14:05:53 +0800

    Upgrade dependencies
    netty          >> 4.1.65.Final
    netty-tcnative >> 2.0.39.Final
    libcommon      >> 2.2.0
    jackson        >> 2.12.4
    slf4j          >> 1.7.31
    lombok         >> 1.18.20
    log4j          >> 2.14.1

--- buildSrc/src/main/kotlin/libnetty.java-library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/libnetty.java-library-conventions.gradle.kts
@@ -11,20 +11,20 @@ repositories {
 
 dependencies {
     // netty-bom
-    api(platform("io.netty:netty-bom:4.1.59.Final"))
+    api(platform("io.netty:netty-bom:4.1.65.Final"))
     // libcommon-bom
-    api(platform("com.github.fmjsjx:libcommon-bom:1.1.1"))
+    api(platform("com.github.fmjsjx:libcommon-bom:2.2.0"))
     // junit-bom
     testImplementation(platform("org.junit:junit-bom:5.7.0"))
     // jackson2-bom
-    api(platform("com.fasterxml.jackson:jackson-bom:2.12.1"))
+    api(platform("com.fasterxml.jackson:jackson-bom:2.12.4"))
 
     constraints {
-        api("org.slf4j:slf4j-api:1.7.30")
-        compileOnly("org.projectlombok:lombok:1.18.16")
-        annotationProcessor("org.projectlombok:lombok:1.18.16")
-        implementation("io.netty:netty-tcnative:2.0.36.Final")
-        implementation("io.netty:netty-tcnative-boringssl-static:2.0.36.Final")
+        api("org.slf4j:slf4j-api:1.7.31")
+        compileOnly("org.projectlombok:lombok:1.18.20")
+        annotationProcessor("org.projectlombok:lombok:1.18.20")
+        implementation("io.netty:netty-tcnative:2.0.39.Final")
+        implementation("io.netty:netty-tcnative-boringssl-static:2.0.39.Final")
         implementation("ch.qos.logback:logback-classic:1.2.3")
         implementation("com.jcraft:jzlib:1.1.3")
         implementation("org.brotli:dec:0.1.2")
@@ -33,7 +33,7 @@ dependencies {
         testImplementation("org.mockito:mockito-inline:3.7.7")
 	}
     // log4j2
-    implementation(platform("org.apache.logging.log4j:log4j-bom:2.14.0"))
+    implementation(platform("org.apache.logging.log4j:log4j-bom:2.14.1"))
 
 }
 
