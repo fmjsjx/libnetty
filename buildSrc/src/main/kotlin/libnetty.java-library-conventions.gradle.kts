9eb28c1e0220bab7950d7885e29db4ac96f3031a
fmjsjx <fmjsjx@163.com>
2021-12-27 15:22:24 +0800

    Upgrade dependencies
    libcommon  2.5.3        -> 2.5.4
    netty      4.1.70.Final -> 4.1.72.Final
    jackson    2.13.0       -> 2.13.1
    log4j2     2.14.0       -> 2.17.0

--- buildSrc/src/main/kotlin/libnetty.java-library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/libnetty.java-library-conventions.gradle.kts
@@ -11,21 +11,19 @@ repositories {
 
 dependencies {
     // netty-bom
-    api(platform("io.netty:netty-bom:4.1.70.Final"))
+    api(platform("io.netty:netty-bom:4.1.72.Final"))
     // libcommon-bom
-    api(platform("com.github.fmjsjx:libcommon-bom:2.5.3"))
+    api(platform("com.github.fmjsjx:libcommon-bom:2.5.4"))
     // junit-bom
     testImplementation(platform("org.junit:junit-bom:5.7.0"))
     // jackson2-bom
-    api(platform("com.fasterxml.jackson:jackson-bom:2.13.0"))
+    api(platform("com.fasterxml.jackson:jackson-bom:2.13.1"))
 
     constraints {
         api("org.slf4j:slf4j-api:1.7.32")
         compileOnly("org.projectlombok:lombok:1.18.22")
         annotationProcessor("org.projectlombok:lombok:1.18.22")
-        implementation("io.netty:netty-tcnative:2.0.44.Final")
-        implementation("io.netty:netty-tcnative-boringssl-static:2.0.44.Final")
-        implementation("ch.qos.logback:logback-classic:1.2.6")
+        implementation("ch.qos.logback:logback-classic:1.2.10")
         implementation("com.jcraft:jzlib:1.1.3")
         implementation("org.brotli:dec:0.1.2")
         implementation("com.aayushatharva.brotli4j:brotli4j:1.6.0")
@@ -34,7 +32,7 @@ dependencies {
         testImplementation("org.mockito:mockito-inline:3.12.4")
 	}
     // log4j2
-    implementation(platform("org.apache.logging.log4j:log4j-bom:2.15.0"))
+    implementation(platform("org.apache.logging.log4j:log4j-bom:2.17.0"))
 
 }
 
