53304262588dbb30884f42037e40cbb25b70e521
fmjsjx <fmjsjx@163.com>
2022-09-13 16:53:59 +0800

    Upgrade dependencies
    ```
    netty      4.1.74.Final > 4.1.81.Final
    libcommon  2.6.2        > 2.6.3
    jackson    2.13.1       > 2.13.4
    junit      5.8.2        > 5.9.0
    mockito    4.3.1        > 4.8.0
    lombok     1.18.22      > 1.18.24
    log4j2     2.17.1       > 2.18.0
    logback    1.2.10       > 1.2.11
    ```

--- buildSrc/src/main/kotlin/libnetty.java-library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/libnetty.java-library-conventions.gradle.kts
@@ -11,23 +11,24 @@ repositories {
 
 dependencies {
     // netty-bom
-    api(platform("io.netty:netty-bom:4.1.74.Final"))
+    api(platform("io.netty:netty-bom:4.1.81.Final"))
     // libcommon-bom
-    api(platform("com.github.fmjsjx:libcommon-bom:2.6.2"))
+    api(platform("com.github.fmjsjx:libcommon-bom:2.6.3"))
     // jackson2-bom
-    api(platform("com.fasterxml.jackson:jackson-bom:2.13.1"))
+    api(platform("com.fasterxml.jackson:jackson-bom:2.13.4"))
     // junit-bom
-    testImplementation(platform("org.junit:junit-bom:5.8.2"))
+    testImplementation(platform("org.junit:junit-bom:5.9.0"))
     // mockito
-    testImplementation(platform("org.mockito:mockito-bom:4.3.1"))
+    testImplementation(platform("org.mockito:mockito-bom:4.8.0"))
     // log4j2
-    implementation(platform("org.apache.logging.log4j:log4j-bom:2.17.1"))
+    implementation(platform("org.apache.logging.log4j:log4j-bom:2.18.0"))
 
     constraints {
         api("org.slf4j:slf4j-api:1.7.36")
-        compileOnly("org.projectlombok:lombok:1.18.22")
-        annotationProcessor("org.projectlombok:lombok:1.18.22")
-        implementation("ch.qos.logback:logback-classic:1.2.10")
+        val lombokVersion = "1.18.24"
+        compileOnly("org.projectlombok:lombok:$lombokVersion")
+        annotationProcessor("org.projectlombok:lombok:$lombokVersion")
+        implementation("ch.qos.logback:logback-classic:1.2.11")
         implementation("com.jcraft:jzlib:1.1.3")
         implementation("org.brotli:dec:0.1.2")
         implementation("com.aayushatharva.brotli4j:brotli4j:1.6.0")
