0feff13df077ff6cc0df1a5dec25bf521b0f74fe
fmjsjx <fmjsjx@163.com>
2022-09-16 17:24:28 +0800

    Upgrade libcommon to 2.7.0

--- buildSrc/src/main/kotlin/libnetty.java-library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/libnetty.java-library-conventions.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
     // netty-bom
     api(platform("io.netty:netty-bom:4.1.81.Final"))
     // libcommon-bom
-    api(platform("com.github.fmjsjx:libcommon-bom:2.6.3"))
+    api(platform("com.github.fmjsjx:libcommon-bom:2.7.0"))
     // jackson2-bom
     api(platform("com.fasterxml.jackson:jackson-bom:2.13.4"))
     // junit-bom
