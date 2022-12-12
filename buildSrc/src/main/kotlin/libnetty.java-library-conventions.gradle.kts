dfd352cb00f4787b3c3239a0281d539cdb75c0a9
fmjsjx <fmjsjx@163.com>
2022-12-12 17:42:07 +0800

    Upgrade dependencies
    ```
    kotlin                > 1.7.22
    kotlinx-coroutines    > 1.6.4
    
    netty    4.1.84.Final > 4.1.85.Final
    jackson  2.13.4       > 2.14.1
    ```

--- buildSrc/src/main/kotlin/libnetty.java-library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/libnetty.java-library-conventions.gradle.kts
@@ -11,17 +11,19 @@ repositories {
 
 dependencies {
     // netty-bom
-    api(platform("io.netty:netty-bom:4.1.84.Final"))
+    api(platform("io.netty:netty-bom:4.1.85.Final"))
     // libcommon-bom
-    api(platform("com.github.fmjsjx:libcommon-bom:2.7.3"))
+    api(platform("com.github.fmjsjx:libcommon-bom:2.8.0-RC1"))
     // jackson2-bom
-    api(platform("com.fasterxml.jackson:jackson-bom:2.13.4.20221013"))
+    api(platform("com.fasterxml.jackson:jackson-bom:2.14.1"))
     // junit-bom
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     // mockito
     testImplementation(platform("org.mockito:mockito-bom:4.8.1"))
     // log4j2
     implementation(platform("org.apache.logging.log4j:log4j-bom:2.19.0"))
+    // kotlin coroutines
+    implementation(platform("org.jetbrains.kotlinx:kotlinx-coroutines-bom:1.6.4"))
 
     constraints {
         api("org.slf4j:slf4j-api:1.7.36")
@@ -32,6 +34,9 @@ dependencies {
         implementation("com.jcraft:jzlib:1.1.3")
         implementation("org.brotli:dec:0.1.2")
         implementation("com.aayushatharva.brotli4j:brotli4j:1.8.0")
+        val kotlinVersion = "1.7.22"
+        implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
 	}
 
 }
