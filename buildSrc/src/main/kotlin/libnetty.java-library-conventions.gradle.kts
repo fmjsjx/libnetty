53f6555d7922e113f086c258b7825b452c625212
fmjsjx <fmjsjx@163.com>
2023-01-18 14:19:56 +0800

    Upgrade dependencies
    ```
    netty      4.1.85.Final > 4.1.87.Final
    libcommon  2.8.0        > 3.0.0-RC1
    junit      5.9.1        > 5.9.2
    mockito    4.8.1        > 5.0.0
    slf4j      1.7.36       > 2.0.6
    logback    1.2.11       > 1.4.5
    ```

--- buildSrc/src/main/kotlin/libnetty.java-library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/libnetty.java-library-conventions.gradle.kts
@@ -11,26 +11,26 @@ repositories {
 
 dependencies {
     // netty-bom
-    api(platform("io.netty:netty-bom:4.1.85.Final"))
+    api(platform("io.netty:netty-bom:4.1.87.Final"))
     // libcommon-bom
-    api(platform("com.github.fmjsjx:libcommon-bom:2.8.0"))
+    api(platform("com.github.fmjsjx:libcommon-bom:3.0.0-RC1"))
     // jackson2-bom
     api(platform("com.fasterxml.jackson:jackson-bom:2.14.1"))
     // junit-bom
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation(platform("org.junit:junit-bom:5.9.2"))
     // mockito
-    testImplementation(platform("org.mockito:mockito-bom:4.8.1"))
+    testImplementation(platform("org.mockito:mockito-bom:5.0.0"))
     // log4j2
     implementation(platform("org.apache.logging.log4j:log4j-bom:2.19.0"))
     // kotlin coroutines
     implementation(platform("org.jetbrains.kotlinx:kotlinx-coroutines-bom:1.6.4"))
 
     constraints {
-        api("org.slf4j:slf4j-api:1.7.36")
+        api("org.slf4j:slf4j-api:2.0.6")
         val lombokVersion = "1.18.24"
         compileOnly("org.projectlombok:lombok:$lombokVersion")
         annotationProcessor("org.projectlombok:lombok:$lombokVersion")
-        implementation("ch.qos.logback:logback-classic:1.2.11")
+        implementation("ch.qos.logback:logback-classic:1.4.5")
         implementation("com.jcraft:jzlib:1.1.3")
         implementation("org.brotli:dec:0.1.2")
         implementation("com.aayushatharva.brotli4j:brotli4j:1.8.0")
