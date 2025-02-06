65a73a5c3f9cfd2db4b1770496f9b33460f11d37
fmjsjx <fmjsjx@163.com>
2025-02-06 17:52:27 +0800

    Speed up buildscript

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,9 +2,20 @@ plugins {
     `kotlin-dsl`
 }
 
+buildscript {
+    repositories {
+        maven {
+            url = uri("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/")
+        }
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
 repositories {
     maven {
         url = uri("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/")
     }
     gradlePluginPortal()
+    mavenCentral()
 }
