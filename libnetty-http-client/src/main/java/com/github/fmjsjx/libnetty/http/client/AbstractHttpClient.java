83518e0e590f09b92401261307864658e3c5dc5d
fmjsjx <fmjsjx@163.com>
2025-04-13 22:29:34 +0800

    Up to version 3.8.0

--- libnetty-http-client/src/main/java/com/github/fmjsjx/libnetty/http/client/AbstractHttpClient.java
+++ libnetty-http-client/src/main/java/com/github/fmjsjx/libnetty/http/client/AbstractHttpClient.java
@@ -88,7 +88,7 @@ public abstract class AbstractHttpClient implements HttpClient {
         public static final AsciiString GZIP_DEFLATE_BR_ZSTD = AsciiString.cached("gzip,deflate,br,zstd");
     }
 
-    protected static final AsciiString DEFAULT_USER_AGENT_VALUE = AsciiString.cached("Libnetty/3.8.0-RC1");
+    protected static final AsciiString DEFAULT_USER_AGENT_VALUE = AsciiString.cached("Libnetty/3.8.0");
 
     protected final EventLoopGroup group;
     protected final Class<? extends Channel> channelClass;
