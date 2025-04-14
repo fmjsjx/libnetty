6d1dfa47b64b674fff40aff7ee99a4b0c6c0d8ee
fmjsjx <fmjsjx@163.com>
2025-04-14 16:53:10 +0800

    Change publish target to local directory

--- buildSrc/src/main/kotlin/libnetty.publish-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/libnetty.publish-conventions.gradle.kts
@@ -9,11 +9,7 @@ version = "3.8.0"
 publishing {
     repositories {
         maven {
-            name = "sonatypeOss"
-            credentials(PasswordCredentials::class)
-            val releasesRepoUrl = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2")
-            val snapshotsRepoUrl = uri("https://oss.sonatype.org/content/repositories/snapshots")
-            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+            url = uri(rootProject.layout.buildDirectory.dir("staging-deploy"))
         }
     }
 }
