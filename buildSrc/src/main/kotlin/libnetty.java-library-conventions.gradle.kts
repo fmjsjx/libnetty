e30ea10adf437ad3fc407a56b5193388b62c798b
fmjsjx <fmjsjx@163.com>
2025-08-21 11:42:03 +0800

    Upgrade dependencies
    ```
    libcommon  3.15.1 > 3.16.0
    junit      5.13.3 > 5.13.4
    mockito    5.18.0 > 5.19.0
    brotli4j   1.18.0 > 1.19.0
    kotlin     2.1.0  > 2.2.0
    jackson    2.19.1 > 2.19.2
    fastjson2  2.0.57 > 2.0.58
    ```

--- buildSrc/src/main/kotlin/libnetty.java-library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/libnetty.java-library-conventions.gradle.kts
@@ -13,21 +13,21 @@ dependencies {
     // netty-bom
     api(platform("io.netty:netty-bom:4.2.4.Final"))
     // libcommon-bom
-    api(platform("com.github.fmjsjx:libcommon-bom:3.15.1"))
+    api(platform("com.github.fmjsjx:libcommon-bom:3.16.0"))
     // jackson2-bom
-    api(platform("com.fasterxml.jackson:jackson-bom:2.19.1"))
+    api(platform("com.fasterxml.jackson:jackson-bom:2.19.2"))
     // junit-bom
-    testImplementation(platform("org.junit:junit-bom:5.13.3"))
+    testImplementation(platform("org.junit:junit-bom:5.13.4"))
     // mockito
-    testImplementation(platform("org.mockito:mockito-bom:5.18.0"))
+    testImplementation(platform("org.mockito:mockito-bom:5.19.0"))
     // log4j2
     implementation(platform("org.apache.logging.log4j:log4j-bom:2.25.0"))
     // kotlin coroutines
     implementation(platform("org.jetbrains.kotlinx:kotlinx-coroutines-bom:1.10.2"))
     // brotli4j
-    implementation(platform("com.aayushatharva.brotli4j:all:1.18.0"))
+    implementation(platform("com.aayushatharva.brotli4j:all:1.19.0"))
     // kotlin
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom:2.1.0"))
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom:2.2.0"))
 
     constraints {
         api("org.slf4j:slf4j-api:2.0.17")
@@ -37,7 +37,7 @@ dependencies {
         implementation("ch.qos.logback:logback-classic:1.5.18")
         implementation("com.jcraft:jzlib:1.1.3")
         implementation("org.brotli:dec:0.1.2")
-        val fastjson2Version = "2.0.57"
+        val fastjson2Version = "2.0.58"
         api("com.alibaba.fastjson2:fastjson2:$fastjson2Version")
         api("com.alibaba.fastjson2:fastjson2-kotlin:$fastjson2Version")
         implementation("com.github.luben:zstd-jni:1.5.7-4")
@@ -50,8 +50,17 @@ val javaVersion = 17
 java {
     withSourcesJar()
     withJavadocJar()
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
     toolchain {
         languageVersion = JavaLanguageVersion.of(javaVersion)
+
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
     }
 }
 
