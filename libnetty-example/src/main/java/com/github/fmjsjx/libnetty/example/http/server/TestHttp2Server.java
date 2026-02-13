f23e181cf647e0f5a470d177ae4f487b6b092eca
fmjsjx <fmjsjx@163.com>
2026-02-13 16:31:27 +0800

    [Feat] Add factory methods on SslContextProviders for HTTP2

--- libnetty-example/src/main/java/com/github/fmjsjx/libnetty/example/http/server/TestHttp2Server.java
+++ libnetty-example/src/main/java/com/github/fmjsjx/libnetty/example/http/server/TestHttp2Server.java
@@ -1,6 +1,5 @@
 package com.github.fmjsjx.libnetty.example.http.server;
 
-import static io.netty.handler.codec.http.HttpMethod.*;
 import com.github.fmjsjx.libnetty.handler.ssl.ChannelSslInitializer;
 import com.github.fmjsjx.libnetty.handler.ssl.SslContextProviders;
 import com.github.fmjsjx.libnetty.http.HttpContentCompressorProvider;
@@ -15,14 +14,13 @@ import com.github.fmjsjx.libnetty.http.server.middleware.AuthBasic;
 import com.github.fmjsjx.libnetty.http.server.middleware.Router;
 import com.github.fmjsjx.libnetty.http.server.middleware.ServeStatic;
 import io.netty.handler.codec.http.cors.CorsConfigBuilder;
-import io.netty.handler.codec.http.websocketx.*;
-import io.netty.handler.codec.http2.Http2SecurityUtil;
-import io.netty.handler.ssl.*;
-import io.netty.pkitesting.CertificateBuilder;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolConfig;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Collections;
 import java.util.Map;
+
+import static io.netty.handler.codec.http.HttpMethod.*;
 
 
 /**
@@ -46,26 +44,7 @@ public class TestHttp2Server {
         var kotlinController = new KotlinController();
         var corsConfig = CorsConfigBuilder.forAnyOrigin().allowedRequestMethods(GET, POST, PUT, PATCH, DELETE)
                 .allowedRequestHeaders("*").allowNullOrigin().build();
-        var ssc = new CertificateBuilder()
-                .subject("cn=localhost")
-                .setIsCertificateAuthority(true)
-                .buildSelfSigned();
-        var sslProvider = SslProvider.isAlpnSupported(SslProvider.OPENSSL_REFCNT) ? SslProvider.OPENSSL_REFCNT : SslProvider.JDK;
-        var sslCtx = SslContextBuilder.forServer(ssc.toKeyManagerFactory())
-                .sslProvider(sslProvider)
-                /* NOTE: the cipher filter may not include all ciphers required by the HTTP/2 specification.
-                 * Please refer to the HTTP/2 specification for cipher requirements. */
-                .ciphers(Http2SecurityUtil.CIPHERS, SupportedCipherSuiteFilter.INSTANCE)
-                .applicationProtocolConfig(new ApplicationProtocolConfig(
-                        ApplicationProtocolConfig.Protocol.ALPN,
-                        // NO_ADVERTISE is currently the only mode supported by both OpenSsl and JDK providers.
-                        ApplicationProtocolConfig.SelectorFailureBehavior.NO_ADVERTISE,
-                        // ACCEPT is currently the only mode supported by both OpenSsl and JDK providers.
-                        ApplicationProtocolConfig.SelectedListenerFailureBehavior.ACCEPT,
-                        ApplicationProtocolNames.HTTP_2,
-                        ApplicationProtocolNames.HTTP_1_1))
-                .build();
-        var sslCtxProvider = SslContextProviders.simple(sslCtx);
+        var sslCtxProvider = SslContextProviders.selfSignedForH2Server();
         var server = new DefaultHttpServer("test", 8443) // server name and port
                 .enableSsl(ChannelSslInitializer.of(sslCtxProvider)) // SSL
                 .corsConfig(corsConfig) // CORS support
