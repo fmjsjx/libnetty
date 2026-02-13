e3d25ab7c47b3369fc1f0ad1ad8126316061bba9
fmjsjx <fmjsjx@163.com>
2026-02-13 11:58:12 +0800

    [Chore] Bump Netty to version 4.1.10.Final

--- buildSrc/src/main/kotlin/libnetty.java-library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/libnetty.java-library-conventions.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 
 dependencies {
     // netty-bom
-    api(platform("io.netty:netty-bom:4.2.9.Final"))
+    api(platform("io.netty:netty-bom:4.2.10.Final"))
     // libcommon-bom
     api(platform("com.github.fmjsjx:libcommon-bom:4.1.0"))
     // jackson2-bom
