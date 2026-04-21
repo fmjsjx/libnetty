7de4ecaeb9f2ff85ca005012e44f87e6f449343c
MJ Fang <fmjsjx@163.com>
2026-04-21 19:43:00 +0800

    [Chore] Bump Log4j to version 2.25.4

--- buildSrc/src/main/kotlin/libnetty.java-library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/libnetty.java-library-conventions.gradle.kts
@@ -23,7 +23,7 @@ dependencies {
     // mockito
     testImplementation(platform("org.mockito:mockito-bom:5.23.0"))
     // log4j2
-    implementation(platform("org.apache.logging.log4j:log4j-bom:2.25.3"))
+    implementation(platform("org.apache.logging.log4j:log4j-bom:2.25.4"))
     // kotlin coroutines
     implementation(platform("org.jetbrains.kotlinx:kotlinx-coroutines-bom:1.10.2"))
     // brotli4j
