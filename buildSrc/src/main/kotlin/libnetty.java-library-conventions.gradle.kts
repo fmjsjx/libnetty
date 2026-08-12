7fc2afff9e5421fde2ef5c6273c63816c7b7579e
MJ Fang <fmjsjx@163.com>
2026-08-12 13:42:54 +0800

    [Chore] Bump Netty to version 4.2.17.Final

--- buildSrc/src/main/kotlin/libnetty.java-library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/libnetty.java-library-conventions.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 
 dependencies {
     // netty-bom
-    api(platform("io.netty:netty-bom:4.2.15.Final"))
+    api(platform("io.netty:netty-bom:4.2.17.Final"))
     // libcommon-bom
     api(platform("com.github.fmjsjx:libcommon-bom:4.2.0"))
     // jackson2-bom
