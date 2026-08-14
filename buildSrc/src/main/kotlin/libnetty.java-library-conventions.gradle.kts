28a32396526f9a5ac6c0385f6122a7b03e3b640d
fmjsjx <fmjsjx@163.com>
2026-08-14 22:34:30 +0800

    [Chore] Bump Jackson2 to version 2.22.1

--- buildSrc/src/main/kotlin/libnetty.java-library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/libnetty.java-library-conventions.gradle.kts
@@ -15,7 +15,7 @@ dependencies {
     // libcommon-bom
     api(platform("com.github.fmjsjx:libcommon-bom:4.3.0-alpha1"))
     // jackson2-bom
-    api(platform("com.fasterxml.jackson:jackson-bom:2.21.3"))
+    api(platform("com.fasterxml.jackson:jackson-bom:2.22.1"))
     // jackson3-bom
     api(platform("tools.jackson:jackson-bom:3.1.3"))
     // junit-bom
